ae53106ed2320abb524b81e8ca3dd5efc7d21e72
anjali vijay <anjalivijay@anjalis-MacBook-Pro.local>
2024-08-05 17:00:00 +0530

    LDT-533-pages-metadata: Page directory meta data.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <Head>
+        <title>This is page title</title>
+        {/* Added key so that it picks latest value otherwise it shows both */}
+        <meta property="description" content="desc 1" key="desc"/>
+      </Head>
+      <h1>Home</h1>
+      <NestedMetadata />
+    </>
+  );
+}
+
+function NestedMetadata() {
+  return (
+    <>
+      <Head>
+        {/* It is the latest value */}
+        <title>This is nested meta data</title>
+        <meta property="description" content="desc 2" key="desc"/>
+      </Head>
+    </>
+  )
 }
